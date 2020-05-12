ee05e65c733f14ebd86066dabb245171032d9182
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-05-12 18:29:06 -0300

    added exercise5.js and modified exercise4.js

--- exercises/4.1/exeercise4.js
+++
@@ -1,9 +0,0 @@
-let num = 5;
-
-if (num > 0) {
-    return "positive";
-} else if (num < 0) {
-    return "negative";
-} else {
-    "zero"
-}
---
+++ exercises/4.1/exercise5.js
@@ -0,0 +1,12 @@
+let lado1 = 60;
+let lado2 = 60;
+let lado3 = 60;
+
+let soma = lado1 + lado2 + lado3;
+
+if ( soma == 180 ) {
+  console.log( "true" );
+} else {
+  console.log( "false" );
+  console.log("A Soma dos lados deve ser igual à 180");
+}
