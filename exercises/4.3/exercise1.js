ca01caab50a3f0482f358b3c5fc26547c8584270
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-05-14 18:12:42 -0300

    modified  exercise1.js

--- exercises/4.3/exercise1.js
+++ exercises/4.3/exercise1.js
@@ -4,9 +4,8 @@ for (let i = 0; i < n; i += 1) {
     if(n > 0 ) {
         linha += "*";
     }
-    for (j = 0; j < n; j += 1) {
-    }
-    console.log(linha);
+  }
+  
+for (j = 0; j <  n; j += 1) {
+  console.log(linha);
 }
-
-
