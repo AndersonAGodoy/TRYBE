7706b51625d1a397bbe55ff9201744742ed2c9cd
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-05-14 13:29:57 -0300

    modified exercise5.js

--- exercises/4.2/exercise5.js
+++ exercises/4.2/exercise5.js
@@ -1,5 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let verificadorNumeroMaior = 0;
+let verificadorNumeroMaior = numbers[0];
 
 for (i = 0; i < numbers.length; i += 1) {
   if(numbers[i] > verificadorNumeroMaior) {
