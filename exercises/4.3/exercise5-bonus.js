7601968302462067f80fbbab036ff843c84f7773
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-05-15 13:58:32 -0300

    Added exercise5-bonus.js and modified exercise4.js

--- exercises/4.3/exercise5-bonus.js
+++ exercises/4.3/exercise5-bonus.js
@@ -1,4 +1,4 @@
-let n = 5;
+let n = 9;
 let numberOfLines = (n + 1) /2 ;
 let controlLeft = numberOfLines;
 let controlRight = numberOfLines;
@@ -7,7 +7,9 @@ let lineInput = "";
 
 for(let i = 0; i < numberOfLines; i += 1 ){
   for(let lineColumn = 1; lineColumn <= n; lineColumn += 1) {
-    if( lineColumn <= controlLeft && lineColumn >= controlRight) {
+    if(i === numberOfLines - 1) {
+        lineInput = lineInput + "*";
+    }else if( lineColumn === controlLeft || lineColumn === controlRight) {
       lineInput = lineInput + "*";
     } else {
       lineInput = lineInput + " ";
