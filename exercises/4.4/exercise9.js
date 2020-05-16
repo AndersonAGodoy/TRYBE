adf89df1b01b985e3f65ba3465f8f4353411fe72
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-05-16 17:45:56 -0300

    Added Exercise10.js, exercise11.js and modified exercise9.js

--- exercises/4.4/exercise9.js
+++ exercises/4.4/exercise9.js
@@ -1,9 +1,13 @@
 let names = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
-let comparador = names[0];
 
-for(let i = 0; i < names.length; i += 1) {
-  if(names[i].length > comparador.length) {
-    comparador = names[i];
+function compararNomes(names){
+  let comparador = names[0];
+  for(let i = 0; i < names.length; i += 1) {
+    if(names[i].length > comparador.length) {
+      comparador = names[i];
+    }
   }
-}
 console.log(comparador);
+}
+
+compararNomes(names);
