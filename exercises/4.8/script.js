96b06fc92c285b30473822ba8ce62a985e8b0072
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-05-22 12:12:48 -0300

    criado as variáveis para manipulação

---
+++ exercises/4.8/script.js
@@ -0,0 +1,8 @@
+let backgroundColorChange;
+let textColorChange;
+let defaultBgColor = localStorage.getItem('backgroundColor');
+let defaultTxtColor = localStorage.getItem('pColor');
+let textSizeInput = document.querySelector('.textSize');
+let textSize = document.querySelector('p');
+let lineHeightInput = document.querySelector('.lineHeight');
+let fontFamilyChangeInput = document.querySelector('.fontFamilyChange');
