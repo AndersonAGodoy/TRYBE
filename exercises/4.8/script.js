f32b3666a97740001c28dbd57cd1b2864f17ff4e
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-05-22 12:19:01 -0300

    adicionado funções que manipulam o colorpicker do html5

--- exercises/4.8/script.js
+++ exercises/4.8/script.js
@@ -33,3 +33,51 @@ function changelineHeight() {
   changeIt();
 } 
 
+function startup() {
+  backgroundColorChange = document.querySelector('.backgroundColor');
+  backgroundColorChange.value = defaultBgColor;
+  backgroundColorChange.addEventListener('input', updateFirst, false);
+  backgroundColorChange.addEventListener('change', updateAll, false);
+  backgroundColorChange.select();
+}
+
+function startup2() {
+  textColorChange = document.querySelector('.textColor');
+  textColorChange.value = defaultTxtColor;
+  textColorChange.addEventListener('input', updateFirst2, false);
+  textColorChange.addEventListener('change', updateAll2, false);
+  textColorChange.select();
+}
+
+
+function updateFirst(event) {
+  let body = document.querySelector('body');
+
+  if (body) {
+    body.style.backgroundColor = event.target.value;
+  }
+}
+
+function updateFirst2(event) {
+  let p = document.querySelector('p');
+
+  if (p) {
+    p.style.color = event.target.value;
+  }
+}
+
+function updateAll(event) {
+  
+  document.querySelectorAll('body').forEach(function (body) {
+    body.style.backgroundColor = event.target.value;
+  });
+  localStorage.setItem('backgroundColor', event.target.value);
+}
+
+function updateAll2(event) {
+  document.querySelectorAll('p').forEach(function (p) {
+    p.style.color = event.target.value;
+  });
+  localStorage.setItem('pColor', event.target.value);
+  
+}
