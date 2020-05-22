9d1695cef2d825c7eb3493e9c1d35de5b33947b5
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-05-22 12:15:37 -0300

    adicionado função para alterar o tamanho do texto

--- exercises/4.8/script.js
+++ exercises/4.8/script.js
@@ -6,3 +6,24 @@ let textSizeInput = document.querySelector('.textSize');
 let textSize = document.querySelector('p');
 let lineHeightInput = document.querySelector('.lineHeight');
 let fontFamilyChangeInput = document.querySelector('.fontFamilyChange');
+
+textSizeInput.addEventListener('change', changeTextSize);
+lineHeightInput.addEventListener('change', changelineHeight);
+fontFamilyChangeInput.addEventListener('change', changeFontFamily);
+
+window.addEventListener('load', startup);
+window.addEventListener('load', startup2);
+window.addEventListener('load', changeIt());
+
+function changeFontFamily() {
+  let newFont = fontFamilyChangeInput.value;
+  localStorage.setItem('fontFamilyFont', newFont);
+  changeIt();
+}
+
+function changeTextSize() {
+  let textSizeValue = textSizeInput.value;
+  localStorage.setItem('fontType', textSizeValue);
+  changeIt();
+}
+
