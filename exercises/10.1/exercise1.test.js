1cd939f8220e1559c7e6e4b7f6ab76f5df56a581
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-07-08 17:44:27 -0300

    exercises 10.1 done

--- exercises/10.1/exercise1.test.js
+++
@@ -1,9 +0,0 @@
-const uppercase = (str, callback) => {
-  callback(str.toUpperCase());
-}
-
-test('Text should be uppercase', () => {
-  uppercase('teste', (callback) => {
-    expect(callback).toBe('TESTE');
-  })
-})
